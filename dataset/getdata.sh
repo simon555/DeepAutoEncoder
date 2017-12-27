aa21db0664f06fbe2336d2fdea9e98d5f45853a3
simon555 <simon.sebbagh@free.fr>
2017-12-26 20:10:15 -0500

    need to modify the download link

--- dataset/getdata.sh
+++ dataset/getdata.sh
@@ -6,6 +6,5 @@ fileURL='https://drive.google.com/uc?export=download&id=1Trr5W7a5h5F6t7iULKZMv7O
 # GloVe
 echo $fileURL
 curl -LO $fileURL
-unrar e data-mvct.rar ./
-mv data-mvct raw3DVolumes
-rm test-file.rar
+unrar e data-mvct.rar raw3DVolumes/
+rm data-mvct.rar
